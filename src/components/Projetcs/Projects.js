0d0e3117988040ac346efc38d3b0252ae1fc9f36
marianacarbonell <marianacarbonellm@gmail.com>
2019-05-09 18:24:02 -0400

    imagen en about y en proyetos con links

--- src/components/Projetcs/Projects.js
+++ src/components/Projetcs/Projects.js
@@ -15,54 +15,54 @@ class Projects extends Component {
                 </div>
                 <div className="row allCards" >
                     <div className="col-sm-6 col-md-3" >
-                        <Card style={{ width: '14rem', height: "33rem" }} className="cardProjects firstCard">
-                            <Card.Img style={{ height: "33rem" }} variant="top" src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/Captura%20de%20pantalla%20de%202019-04-16%2014-18-08.jpg" />
+                        <Card style={{ width: '14rem', height: "31rem" }} className="cardProjects firstCard">
+                            <Card.Img style={{ width: '100%', height: "200px" }} variant="top" src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/Captura%20de%20pantalla%20de%202019-04-16%2014-18-08.jpg" />
                             <Card.Body className="Card.Body">
                                 <Card.Title><h4 style={{ textAlign: 'center', fontWeight: "bolder" }}>Movie Time</h4></Card.Title>
                                 <Card.Text className="Card.Text text-justify">
                                     Movie time es una aplicación que te da recomendaciones de películas o series según el tiempo que tú tengas disponibles para ver.
                 </Card.Text>
-                                <a href="https://github.com/marianacarbonell/SCL007-hackathon-peliculas" className="btnCard" variant="primary">Repositorio</a><br></br>
-                                <a href="" className="btnCard" variant="primary">Demo</a>
+                                <a href="https://github.com/marianacarbonell/SCL007-hackathon-peliculas" target="_blank" className="btnCard" variant="primary">Repositorio</a><br></br>
+                                <a href="" target="_blank" className="btnCard" variant="primary">Demo</a>
                             </Card.Body>
                         </Card>
                     </div>
                     <div className="col-sm-6 col-md-3">
-                        <Card style={{ width: '14rem', height: "33rem" }} className="cardProjects">
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card style={{ width: '14rem', height: "31rem" }} className="cardProjects">
+                            <Card.Img style={{ width: '100%', height: "200px" }} variant="top" src="holder.js/100px180" />
                             <Card.Body className="Card.Body">
                                 <Card.Title ><h4 style={{ textAlign: 'center', fontWeight: "bolder" }}>BurgerQueen</h4></Card.Title>
                                 <Card.Text className="Card.Text text-justify">
                                     BurgerQueen es una interfaz hecha para una cadena de comida rápida, con la cual puedan tomar pedidos usando una tablet.
                                                 </Card.Text>
-                                <a href="" className="btnCard" variant="primary">Repositorio</a><br></br>
-                                <a href="" className="btnCard" variant="primary">Demo</a>
+                                <a href="https://github.com/marianacarbonell/SCL007-BurgerQueen/tree/develop" target="_blank" className="btnCard" variant="primary">Repositorio</a><br></br>
+                                <a href="" target="_blank" className="btnCard" variant="primary">Demo</a>
                             </Card.Body>
                         </Card>
                     </div>
                     <div className="col-sm-6 col-md-3">
-                        <Card style={{ width: '14rem', height: "33rem" }} className="cardProjects">
-                            <Card.Img variant="top" src="../../../Bocetos/Captura de pantalla de 2019-04-16 14-18-08.jpgaa"/>
+                        <Card style={{ width: '14rem', height: "31rem" }} className="cardProjects">
+                            <Card.Img style={{ width: '100%', height: "200px" }} variant="top" src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/pinterest.jpg"/>
                             <Card.Body className="Card.Body">
                                 <Card.Title><h4 style={{ textAlign: 'center', fontWeight: "bolder" }}>Pinterest</h4></Card.Title>
                                 <Card.Text className="Card.Text text-justify">
                                     Replica del diseño de la página pinterest, hecha en versión desktop
                 </Card.Text>
-                                <a href="" className="btnCard" variant="primary">Repositorio</a><br></br>
-                                <a href="" className="btnCard" variant="primary">Demo</a>
+                                <a href="https://github.com/marianacarbonell/pinterest"  target="_blank" className="btnCard" variant="primary">Repositorio</a><br></br>
+                                <a href="https://marianacarbonell.github.io/pinterest/" target="_blank" className="btnCard" variant="primary">Demo</a>
                             </Card.Body>
                         </Card>
                     </div>
                     <div className="col-sm-6 col-md-3">
-                        <Card style={{ width: '14rem', height: "33rem" }} className="cardProjects">
-                            <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card style={{ width: '14rem', height: "31rem" }} className="cardProjects">
+                            <Card.Img style={{ width: '100%', height: "200px" }} variant="top" src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/social%20pets.jpg" />
                             <Card.Body className="Card.Body">
                                 <Card.Title><h4 style={{ textAlign: 'center', fontWeight: "bolder" }}>Social Pets</h4></Card.Title>
                                 <Card.Text className="Card.Text text-justify">
                                     Social Pets es red social para dueños de mascotas, que da información sobre salud y alimentación.
                 </Card.Text>
-                                <a href="" className="btnCard" variant="primary">Repositorio</a><br></br>
-                                <a href="" className="btnCard" variant="primary">Demo</a>
+                                <a href="" className="btnCard" target="_blank" variant="primary">Repositorio</a><br></br>
+                                <a href="https://milexys.github.io/SCL007-Social-Network/" target="_blank" className="btnCard" variant="primary">Demo</a>
                             </Card.Body>
                         </Card>
                     </div>
--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,13 +11,12 @@ class About extends Component {
                 <div className="title2">
                     <h3 style={{color: 'white'}}><b>Sobre mí</b></h3>
                     </div>
-                    <div className="card mb-3 cardAbout" style={{ maxWidth: '50%' }}>
-                        <div class="row no-gutters">
-
-                            <div class="col-md-4">
-                                <img src="http://img.imgur.com/Y0ONI.jpg" class="card-img" alt="..." />
+                    <div className="card mb-3 cardAbout" style={{ maxWidth: '50%'}}>
+                    <div class="image" style={{width:'100%', margin:"auto"}}>
+                                <img  style={{ width: '70%', height: "70%"}} src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/Mariana%20Carbonell.JPG" class="card-img,  rounded-circle" alt="..." />
                             </div>
-                            <div class="col-md-8">
+                        <div class="row no-gutters">
+                            <div class="col-md-12">
                                 <div class="card-body">
                                     <h2 class="card-title"><b>Mariana Carbonell</b></h2>
                                     <h6 class="card-title"><b>Developer Front-end(Laboratoria)</b></h6>
