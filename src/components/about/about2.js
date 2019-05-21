b436a05ad452acadd63b8284b67709c55ea1001a
marianacarbonell <marianacarbonellm@gmail.com>
2019-05-21 19:52:56 -0400

    imagen en el principio, texto sobre mi listo

--- src/components/about/about2.js
+++ src/components/about/about2.js
@@ -7,10 +7,13 @@ class About2 extends Component {
     render() {
         return (
             <div id="about2" class="home">
-            <div id="tittle1" style={{color: 'white'}}> </div>
-            <div id="name" style={{color: 'white'}}> Mariana Carbonell</div>
-            <div id="" class="profession" style={{color: 'white'}}> Front-end Developer | Diseñadora Gráfica</div>
-            </div>
+                <div id="tittle1" style={{ color: 'white' }}> </div>
+                <div id="name" style={{ color: 'white' }}> Mariana Carbonell</div>
+                <div id="" class="profession" style={{ color: 'white' }}> Front-end Developer | Diseñadora Gráfica</div>
+                <div className="photo" >
+                            <img style={{ width: '900px', height: "auto" }} src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/Mariana%20CarbonellPortafolio.png" />
+                        </div>
+                        </div>
         )
     }
 }
