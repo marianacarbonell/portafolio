ad272953e104092899d6fe93de967dc15d4d5c9c
marianacarbonell <marianacarbonellm@gmail.com>
2019-06-03 20:35:29 -0400

    probando

--- src/components/ME/me.js
+++ src/components/ME/me.js
@@ -12,8 +12,7 @@ class Me extends Component {
                 
             <p class="meText">Constantemente me encuentro en la búsqueda de nuevos conocimientos,con el fin de mejorar y crecer tanto profesional como personalmente. </p>
             <p class="meText2">Me considero totalmente enamorada de la programación y con muchas ganas de seguir aprendiendo. Mi objetivo es poder integrar mis conocimientos de diseño gráfico con los de programación. Además tengo las herramientas necesarias de auto-aprendizaje y metodologías ágiles para seguir creciendo en ambos ámbitos.</p>
-           <img className="photoMe" src="https://raw.githubusercontent.com/marianacarbonell/portafolio/master/Bocetos/sirena.jpg" />
-          
+           
 
             </div>
         )
