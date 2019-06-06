33aa1bfdd90d5f063bd4907159ad331fdc3e815d
marianacarbonell <marianacarbonellm@gmail.com>
2019-06-06 16:17:45 -0400

    Updates

---
+++ precache-manifest.3d402628fbb64bf64d361f9e2da8c46c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fd29bcc1d4ba73bed23",
+    "url": "/portafolio/static/js/runtime~main.41300385.js"
+  },
+  {
+    "revision": "990f90f9f343e2093355",
+    "url": "/portafolio/static/js/main.4441ac24.chunk.js"
+  },
+  {
+    "revision": "713ecba404dda74d8e8e",
+    "url": "/portafolio/static/js/2.430f62ce.chunk.js"
+  },
+  {
+    "revision": "990f90f9f343e2093355",
+    "url": "/portafolio/static/css/main.7ee5acde.chunk.css"
+  },
+  {
+    "revision": "7694774ef5d46382362a9b06a46d86eb",
+    "url": "/portafolio/index.html"
+  }
+];
