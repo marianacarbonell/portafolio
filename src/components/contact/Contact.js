c5072ba602a7c59c2159f352a3667ea0e22efa9d
marianacarbonell <marianacarbonellm@gmail.com>
2019-06-07 08:59:07 -0400

    logo de behance en los contactos

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -11,6 +11,7 @@ class Contact extends Component {
                     <a href="https://www.linkedin.com/in/mariana-carbonell-machado-b25518156/" target="_blank" className="git"><i class="fab fa-linkedin-in linkedin"></i></a>
                     <a href="mailto:marianacarbonellm@gmail.com" target="_blank" className="git"><i class="fas fa-envelope mail"></i></a>
                     <a href="https://wa.me/56984867112" target="_blank" className="wsp"><i class="fab fa-whatsapp"></i></a>
+                    <a href="https://www.behance.net/unacarbonell" target="_blank" className="behance"><i class="fab fa-behance"></i></a>
                 </div>
             </div>
         )
